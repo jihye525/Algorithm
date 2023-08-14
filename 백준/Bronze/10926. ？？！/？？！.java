19640b580bc7ceb41583156da6cac82a2812e283
jihye525 <104695986+jihye525@users.noreply.github.com>
2023-08-14 11:37:37 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+    Scanner key = new Scanner(System.in);
+    String id = key.next();
+   
+    System.out.println(id + "??!");
+    
+}
+}
