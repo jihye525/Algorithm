ad755f9bd13c0396aec2e3662c478a74f907b996
jihye525 <104695986+jihye525@users.noreply.github.com>
2023-08-14 19:34:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner key = new Scanner(System.in);
+        long num1 = key.nextLong();
+        long num2 = key.nextLong();
+        long num3 = key.nextLong();
+        
+        System.out.println(num1 + num2 + num3);
+    }
+}
