4fd3b3ec9bd23f24d0b26c6932dcf91e474ce7b2
jihye525 <104695986+jihye525@users.noreply.github.com>
2024-06-06 14:38:58 +0900

    [Bronze III] Title: 홀수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -0,0 +1,12 @@
+lst = []
+
+for _ in range(7):
+    a = int(input())
+    if a % 2 == 1:
+        lst.append(a)
+        
+if len(lst) == 0:
+    print(-1)
+else:
+    print(sum(lst))
+    print(min(lst))
