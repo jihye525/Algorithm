1dabf9269a69f7004210cc115f0753d17f2b9693
jihye525 <104695986+jihye525@users.noreply.github.com>
2024-07-27 22:55:59 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 840 ms, Memory: 93316 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,18 +1,22 @@
 import sys
 input = sys.stdin.readline
 str = input().rstrip()
-chr_idx_lst = [[] for _ in range(26)]
 n = int(input())
-for i in range(len(str)):
-    chr_idx_lst[ord(str[i])-97].append(i)
+chr_total_lst = [[0] * 26]
+
+chr_total_lst[0][ord(str[0])-97] = 1
+
+for i in range(1, len(str)):
+    c = ord(str[i])-97
+    temp = chr_total_lst[i-1].copy()
+    temp[c] = chr_total_lst[i-1][c] + 1
+    chr_total_lst.append(temp)
+
 for _ in range(n):
     alpha, s, e = input().split()
     s, e = int(s), int(e)
-    temp = chr_idx_lst[ord(alpha)-97]
-    ans = 0
-    for idx in temp:
-        if s <= idx <= e:
-            ans += 1
-        elif idx > e:
-            break
-    print(ans)
+    if s == 0:
+        print(chr_total_lst[e][ord(alpha)-97])
+    else:
+        print(chr_total_lst[e][ord(alpha)-97]-chr_total_lst[s-1][ord(alpha)-97])
+        
