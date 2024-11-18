191968916b1abc61cadf8043ddd9b78598005dda
jihye525 <104695986+jihye525@users.noreply.github.com>
2024-11-18 10:10:44 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,25 @@
 def solution(brown, yellow):
-    for i in range(1, int(yellow ** 0.5) + 1):
+    if yellow == 1:
+        return [3, 3]
+    
+    if isPrime(yellow):
+        return [yellow + 2, 3]
+    
+    for i in range(2, int(yellow ** 0.5) + 1):
         if yellow % i == 0:
             width = (yellow / i) + 2
             height = i + 2
             if width - 1 + height - 1 == brown / 2 :
-                return [width, height]
+                return [width, height]           
+
+def isPrime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
