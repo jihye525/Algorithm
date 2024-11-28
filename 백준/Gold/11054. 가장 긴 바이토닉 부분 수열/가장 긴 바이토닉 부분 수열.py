b98b87dae9d3f61bdf8355a25d44c2f215af38c6
jihye525 <104695986+jihye525@users.noreply.github.com>
2024-11-28 21:12:26 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 232 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,20 +1,15 @@
-x = int(input())
+n = int(input())
+lst = list(map(int, input().split()))
+reverse_lst = lst[::-1]
+dp_up = [1 for _ in range(n)]
+dp_down = [0 for _ in range(n)]
 
-case = list(map(int, input().split()))
-reverse_case = case[::-1]
-
-increase = [1 for i in range(x)] # 가장 긴 증가하는 부분 수열
-decrease = [1 for i in range(x)] # 가장 긴 감소하는 부분 수열(reversed)
-
-for i in range(x):
+for i in range(n):
     for j in range(i):
-        if case[i] > case[j]:
-            increase[i] = max(increase[i], increase[j]+1)
-        if reverse_case[i] > reverse_case[j]:
-            decrease[i] = max(decrease[i], decrease[j]+1)
+        if lst[j] < lst[i]:
+            dp_up[i] = max(dp_up[i], dp_up[j] + 1)
+        if reverse_lst[j] < reverse_lst[i]:
+            dp_down[i] = max(dp_down[i], dp_down[j] + 1)
 
-result = [0 for i in range(x)]
-for i in range(x):
-    result[i] = increase[i] + decrease[x-i-1] -1
 
-print(max(result))
+print(max([dp_up[i] + dp_down[n-1-i] for i in range(n)]))
