5d982a84b17323854b06df45f65bb65f988bfff8
jihye525 <104695986+jihye525@users.noreply.github.com>
2025-08-01 17:19:00 +0900

    [Gold II] Title: 가운데를 말해요, Time: 240 ms, Memory: 39016 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -3,16 +3,19 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-left_heap = []  
-right_heap = []  
+left_heap = []
+right_heap = []
 
 for _ in range(n):
     num = int(input())
-
+    
+    # 먼저 왼쪽에 넣기
     heapq.heappush(left_heap, -num)
-
+    
+    #숫자 맞추기 위해 오른쪽 힙으로 push
     heapq.heappush(right_heap, -heapq.heappop(left_heap))
-
+    
+    # 항상 왼쪽이 오른쪽보다 하나 크거나 같아야 함 
     if len(left_heap) < len(right_heap):
         heapq.heappush(left_heap, -heapq.heappop(right_heap))
 
