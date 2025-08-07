be67b20b45f200d86bdf4de0ff0ce9e0fd716bf1
jihye525 <104695986+jihye525@users.noreply.github.com>
2025-08-07 21:26:47 +0900

    [Gold IV] Title: 타임머신, Time: 464 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -1,28 +1,31 @@
 import sys
 input = sys.stdin.readline
-N, M = map(int, input().split())
-edges = list(tuple(map(int, input().split())) for _ in range(M))
-INF = 5000000
-distance = [0, 0] + [INF] * (N-1)
 
-for i in range(N-1):
-    for j in range(M):
-        s, e, t = edges[j]
-        if distance[s] != INF and distance[e] > distance[s] + t:
-            distance[e] = distance[s] + t
+INF = int(1e18)
+n, m = map(int, input().split())
 
-negative_cycle = False # 무한 음수 사이클 여부 체크
+edges = []
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((a, b, c))
 
-for i in range(M):
-    s, e, t = edges[i]
-    if distance[s] != INF and distance[e] > distance[s] + t:
-        negative_cycle = True
+distance = [INF] * (n + 1)
+distance[1] = 0 
 
-if negative_cycle:
-    print(-1)
+def bellman_ford():
+    for i in range(n - 1):
+        for u, v, w in edges:
+            if distance[u] != INF and distance[v] > distance[u] + w:
+                distance[v] = distance[u] + w
+
+    for u, v, w in edges:
+        if distance[u] != INF and distance[v] > distance[u] + w:
+            return False  # 음수 사이클 있음
+
+    return True
+
+if bellman_ford():
+    for i in range(2, n + 1):
+        print(distance[i] if distance[i] != INF else -1)
 else:
-    for d in distance[2:]:
-        if d == INF:
-            print(-1)
-        else:
-            print(d)
+    print(-1)
