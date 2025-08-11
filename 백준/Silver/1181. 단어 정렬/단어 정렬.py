b993d187b47393c94b8b1e9c181473c0835b4076
jihye525 <104695986+jihye525@users.noreply.github.com>
2025-08-11 21:34:39 +0900

    [Silver V] Title: 단어 정렬, Time: 88 ms, Memory: 37532 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,13 +1,9 @@
 import sys
 input = sys.stdin.readline
-n = int(input())
-lst = []
-for i in range(n):
-    istr = input().rstrip()
-    t = (istr, len(istr))
-    if t not in lst:
-        lst.append(t)
-lst.sort(key=lambda x: (x[1], x[0]))
 
-for x, y in lst:
-    print(x)
+n = int(input())
+words = set(input().strip() for _ in range(n))
+words = list((len(i), i) for i in words)
+words.sort(key=lambda w: (w[0], w[1]))
+for w in words:
+    print(w[1])
