ffbf61609b007152a58a989966f4b1083d153ca5
jihye525 <104695986+jihye525@users.noreply.github.com>
2025-08-12 16:03:40 +0900

    [Silver III] Title: N과 M (7), Time: 1968 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,17 @@
+
+def recur(number):
+    if number == M:
+        print(*arr)
+        return
+
+    for i in lst:
+        arr.append(i)
+        recur(number + 1)
+        arr.pop()
+
+
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
+lst.sort()
+arr = []
+recur(0)
