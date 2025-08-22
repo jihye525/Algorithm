63c38dad9747bcc70decd91a686d474e623aa0b2
jihye525 <104695986+jihye525@users.noreply.github.com>
2025-08-22 16:12:00 +0900

    [Gold V] Title: 토마토, Time: 1608 ms, Memory: 50416 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,54 +1,46 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-m, n, h = map(int, input().split())
 
-tomato_box = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
-visited = [[[0] * m for _ in range(n)] for _ in range(h)]
-dr = [0, 0, 0, 1, 0, -1] #행
-dc = [0, 0, 1, 0, -1, 0] #열
-dh = [-1, 1, 0, 0, 0, 0] #높이
+n, m, h = map(int, input().split())
+boxes = [list(list(map(int, input().split())) for _ in range(m)) for _ in range(h)]
+
+tomatoes = deque()
+for i in range(h):
+    for j in range(m):
+        for k in range(n):
+            if boxes[i][j][k] == 1:
+                tomatoes.append((i, j, k))
 
 
 def bfs():
-    queue = deque()
-    all_even = 1
-    for i in range(h):
-        for j in range(n):
-            for k in range(m):
-                if tomato_box[i][j][k] == 1 and not visited[i][j][k]:
-                    visited[i][j][k] = 1
-                    queue.append((i, j, k))
-                if tomato_box[i][j][k] == 0:
-                    all_even = 0
+    global tomatoes, days
+    temp = deque()
+    while tomatoes:
+        k, r, c = tomatoes.popleft()
+        for dr, dc, dh in ((1,0,0), (0,1,0), (-1, 0, 0), (0, -1, 0), (0,0,1), (0, 0, -1)):
+            nr, nc, nh = r + dr, c + dc, k + dh
+            if 0<=nr<m and 0<=nc<n and 0 <= nh < h and boxes[nh][nr][nc] == 0:
+                boxes[nh][nr][nc] = 1
+                temp.append((nh, nr, nc))
 
-    if all_even:
-        print(0)
-        return
-
-    while queue:
-        i, j, k = queue.popleft()
-        for a in range(6):
-            dj = j + dr[a]
-            dk = k + dc[a]
-            di = i + dh[a]
-            if (0 > dj or dj >= n) or (0 > dk or dk >= m) or (0 > di or di >= h):
-                continue
-            if tomato_box[di][dj][dk] == 0 and not visited[di][dj][dk]:
-                visited[di][dj][dk] = 1
-                queue.append((di, dj, dk))
-                tomato_box[di][dj][dk] = tomato_box[i][j][k] + 1
-
-    answer = 0
-    for a in tomato_box:
-        for b in a:
-            for c in b:
-                if c == 0:
-                    print(-1)
-                    exit(0)
-            answer = max(answer, max(b))
-
-    print(answer - 1)
+    if len(temp) > 0:
+        tomatoes = temp
+        days += 1
+        return True
+    else:
+        return False
 
 
-bfs()
+days = 0
+while True:
+    if not bfs():
+        break
+
+for box in boxes:
+    for row in box:
+        for t in row:
+            if t == 0:
+                days = -1
+
+print(days)
